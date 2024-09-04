c030584c82e11b54330f1d23467639e2605f35e2
stap1718 <stapletonp@cbchs.org>
2024-09-04 13:53:15 -0500

    Update lists_practice.py

--- lists_practice.py
+++ lists_practice.py
@@ -1,1 +1,12 @@
-
+"""
+create alist that includes the full names of at least 6 members in the class.
+Task 1: print the list
+Task 2: access and print only the fourth item in the list
+Task 3:create a variable named message and assign a greeting to it that uses the 2nd item in the list only. Then print the message.
+Task 4: Add a new name to the list
+Task 5: Insert a new name in the 4th position
+Task 6: remove the second item in the list using the del statement
+Task 7: remove the second item using the remove function
+Task 8: sort your list
+Task 9: print the length of your list
+"""
